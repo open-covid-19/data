f1c6303ece6732385ee0f73a9d95371f58ef713e
Oscar Wahltinez <oscar@wahltinez.org>
2020-03-26 00:35:44 -0400

    Fix duplicate records for FR

--- input/parse_opencovid19-fr_fr.py
+++ input/parse_opencovid19-fr_fr.py
@@ -14,7 +14,7 @@ from pathlib import Path
 from datetime import datetime
 
 from numpy import unique
-from pandas import DataFrame
+from pandas import DataFrame, isna
 
 from utils import github_raw_dataframe, dataframe_output
 
@@ -30,6 +30,17 @@ df = df.rename(columns={'date': 'Date', 'cas_confirmes': 'Confirmed', 'deces': '
 # Filter rows to those that are region-level
 df = df[df['granularite'] == 'region']
 df['_RegionLabel'] = df['maille_nom']
+
+# There may be more than one row per region if it has multiple sources
+g = df[['Date', '_RegionLabel', 'Confirmed', 'Deaths']].groupby(['Date', '_RegionLabel'])
+
+# Retrieve the first non-null value per group
+def first_non_null(rows):
+    for row in rows:
+        if not isna(row): return row
+    return rows.iloc[0]
+df = g.agg(first_non_null).reset_index()
+# print(df)
 
 # Output the results
 dataframe_output(df, ROOT, 'fr')
