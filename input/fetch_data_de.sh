aea36bbd956434df6f2044a9db4cc9870c7cd14e
Oscar Wahltinez <oscar@wahltinez.org>
2020-03-25 20:16:02 -0400

    Add data source for Germany

---
+++ input/fetch_data_de.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Use @jgehrcke GitHub project to parse the state-level Germany data
+
+BASE_DIR=`dirname "$0"`
+python "$BASE_DIR/parse_jgehrcke_de.py"
---
+++ input/parse_jgehrcke_de.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+
+'''
+This script loads the latest CSV from
+https://github.com/jgehrcke/covid-19-germany-gae and extracts the confirmed
+cases, deaths and total tests for each subregion in Germany.
+
+Credit to @jgehrcke + contributors for scraping the data.
+'''
+
+import os
+import sys
+from pathlib import Path
+from datetime import datetime
+
+from numpy import unique
+from pandas import DataFrame
+
+from utils import github_raw_dataframe, dataframe_output
+
+# Root path of the project
+ROOT = Path(os.path.dirname(__file__)) / '..'
+
+# Read CSV file from GitHub project
+df = github_raw_dataframe('jgehrcke/covid-19-germany-gae', 'data.csv')
+
+# Rename the appropriate columns
+df = df.rename(columns={'time_iso8601': 'Date'})
+
+# Convert dates to ISO format
+df['Date'] = df['Date'].apply(lambda date: datetime.fromisoformat(date).date().isoformat())
+
+# Get a list of all region codes
+regions = unique([col[3:5] for col in df.columns if col.startswith('DE-')])
+
+# Transform the data from non-tabulated format to our record format
+records = []
+for idx, row in df.iterrows():
+    record = {'Date': row['Date'], 'CountryCode': 'DE'}
+    for region in regions:
+        records.append({
+            'RegionCode': region,
+            'Confirmed': row['DE-%s_cases' % region],
+            'Deaths': row['DE-%s_deaths' % region],
+            **record
+        })
+df = DataFrame.from_records(records)
+
+# Output the results
+dataframe_output(df, ROOT, 'de')
