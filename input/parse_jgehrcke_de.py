7a3271ced8c8397406a3817829fd7f4e1363a864
Oscar Wahltinez <oscar@wahltinez.org>
2020-04-02 18:47:09 -0400

    Fix DE data source and update data

--- input/parse_jgehrcke_de.py
+++ input/parse_jgehrcke_de.py
@@ -24,21 +24,24 @@ df = df.rename(columns={'time_iso8601': 'Date'})
 # Convert dates to ISO format
 df['Date'] = df['Date'].apply(lambda date: datetime.fromisoformat(date).date().isoformat())
 
-# Get a list of all region codes
+# Get a list of all regions
 regions = unique([col[3:5] for col in df.columns if col.startswith('DE-')])
 
 # Transform the data from non-tabulated format to our record format
 records = []
 for idx, row in df.iterrows():
-    record = {'Date': row['Date'], 'CountryCode': 'DE'}
-    for region in regions:
+    record = {'Date': row['Date']}
+    for region_code in regions:
         records.append({
-            'RegionCode': region,
-            'Confirmed': row['DE-%s_cases' % region],
-            'Deaths': row['DE-%s_deaths' % region],
+            'RegionCode': region_code,
+            'Confirmed': row['DE-%s_cases' % region_code],
+            'Deaths': row['DE-%s_deaths' % region_code],
             **record
         })
 df = DataFrame.from_records(records)
+
+# Ensure we only take one record from the table
+df = df.groupby(['Date', 'RegionCode']).last().reset_index()
 
 # Output the results
 dataframe_output(df, 'DE')
--- input/update_data.sh
+++ input/update_data.sh
@@ -14,14 +14,17 @@ rm -rf "$BASE_DIR"/../output/*.csv "$BASE_DIR"/../output/*.json
 # TMP: Avoid deleting the old data.csv file in case someone was using it
 mv "$BASE_DIR"/../output/data.csv.bak "$BASE_DIR"/../output/data.csv
 
-# Run all fetch scripts from the input folder
+# Run all fetch scripts from the input folder in parallel
 TMP_FILE=`mktemp`
 OUT_FILE="$BASE_DIR/../output/data_minimal.csv"
 cat "$BASE_DIR/output_columns.csv" > $OUT_FILE
 FETCH_SCRIPT_LIST="$BASE_DIR/fetch_data_*.sh"
 for FETCH_SCRIPT in $FETCH_SCRIPT_LIST; do
-    sh $FETCH_SCRIPT >> $TMP_FILE
+    sh $FETCH_SCRIPT >> $TMP_FILE &
 done
+
+# Wait until all fetch scripts finish
+wait
 
 # Aggregate all outputs into a single CSV file
 sort -u $TMP_FILE >> $OUT_FILE
