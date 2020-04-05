9f5ef551300443f146cc40b36c03e8faef991f02
Oscar Wahltinez <oscar@wahltinez.org>
2020-04-05 13:12:49 -0400

    Update AR fetch script

--- input/fetch_data_ar.sh
+++ input/fetch_data_ar.sh
@@ -3,4 +3,9 @@
 # Use Wikipedia data to fetch and parse the region-level AR data
 
 BASE_DIR=`dirname "$0"`
-python "$BASE_DIR/parse_wikipedia_ar.py"
+python "$BASE_DIR"/parse_wikipedia_country.py AR \
+    --date_format '%d %b' \
+    --table_index 0 \
+    --skip_head 1 \
+    --cumsum true \
+    --drop_rows 'Date'
