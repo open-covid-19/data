b8174c743d36d2429880a080ba87c36b28580e06
Oscar Wahltinez <oscar@wahltinez.org>
2020-04-04 22:42:26 -0400

    Add BO data source

---
+++ input/fetch_data_bo.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Use Wikipedia data to fetch and parse the region-level BO data
+
+BASE_DIR=`dirname "$0"`
+python "$BASE_DIR"/parse_wikipedia_country.py BO \
+    --date_format '%b %d' \
+    --table_index 1 \
+    --skip_head 1 \
+    --drop_rows 'Date(2020)'
