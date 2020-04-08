9d5fc09c51df2ddec7c9a2d15ef7d6e3152949d9
Oscar Wahltinez <oscar@wahltinez.org>
2020-04-08 12:32:06 -0400

    Update data

--- input/fetch_data_in.sh
+++ input/fetch_data_in.sh
@@ -3,10 +3,11 @@
 # Use Wikipedia data to fetch and parse the region-level IN data
 
 BASE_DIR=`dirname "$0"`
-URL="https://en.wikipedia.org/wiki/Template:2019–20_coronavirus_pandemic_data/India_medical_cases"
+# URL="https://en.wikipedia.org/wiki/Template:2019–20_coronavirus_pandemic_data/India_medical_cases"
+URL="https://en.wikipedia.org/wiki/2020_coronavirus_pandemic_in_India"
 SNAPSHOT=$(python "$BASE_DIR"/download_snapshot.py "$URL" --extension html $@)
 python "$BASE_DIR"/parse_wikipedia_country.py "$SNAPSHOT" \
     --country-code IN \
     --date-format '%b-%d' \
-    --table-index 0 \
+    --table-index 2 \
     --skiprows 1
