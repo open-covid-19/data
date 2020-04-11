465acba594183bb75dc82701e46a63df6ce57f6b
Oscar Wahltinez <oscar@wahltinez.org>
2020-04-11 11:23:57 -0400

    Update data

--- input/fetch_data_kr.sh
+++ input/fetch_data_kr.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Use Wikipedia data to fetch and parse the region-level AR data
+# Use Wikipedia data to fetch and parse the region-level KR data
 
 BASE_DIR=`dirname "$0"`
 URL="https://en.wikipedia.org/wiki/Template:2019%E2%80%9320_coronavirus_pandemic_data/South_Korea_medical_cases"
@@ -8,5 +8,5 @@ SNAPSHOT=$(python "$BASE_DIR"/download_snapshot.py "$URL" --extension html $@)
 python "$BASE_DIR"/parse_wikipedia_country.py "$SNAPSHOT" \
     --country-code KR \
     --date-format '%Y-%m-%d' \
-    --table-index 2 \
+    --table-index 3 \
     --skiprows 1
