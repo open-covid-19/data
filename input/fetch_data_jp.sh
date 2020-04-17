0ff3f8958e7210a3e8ca8d835f833209fdbc11c8
Oscar Wahltinez <oscar@wahltinez.org>
2020-04-17 10:56:02 -0400

    Update data

--- input/fetch_data_jp.sh
+++ input/fetch_data_jp.sh
@@ -3,6 +3,6 @@
 # Use GitHub data to fetch and parse the region-level JP data
 
 BASE_DIR=`dirname "$0"`
-URL="https://raw.github.com/swsoyee/2019-ncov-japan/master/Data/byDate.csv"
+URL="https://raw.github.com/swsoyee/2019-ncov-japan/master/50_Data/byDate.csv"
 SNAPSHOT=$(python "$BASE_DIR"/download_snapshot.py "$URL" $@)
 python "$BASE_DIR"/parse_jp_2019-ncov-japan.py "$SNAPSHOT"
