c3ef005770cf99c631d9dd576d9ca0d9d5c29b47
Oscar Wahltinez <oscar@wahltinez.org>
2020-05-05 20:19:19 -0400

    Rollback non-monotonic data source for ES

--- input/fetch_data_es.sh
+++ input/fetch_data_es.sh
@@ -3,6 +3,10 @@
 # Parse the datadista data from Github for both country and region-level data
 
 BASE_DIR=`dirname "$0"`
-URL="https://covid19.isciii.es/resources/serie_historica_acumulados.csv"
-SNAPSHOT=$(python "$BASE_DIR"/download_snapshot.py "$URL" $@)
-python "$BASE_DIR/parse_es_iscii.py" "$SNAPSHOT"
+URL_BASE="https://raw.github.com/datadista/datasets/master"
+REGION1=$(python "$BASE_DIR"/download_snapshot.py "$URL_BASE/COVID%2019/ccaa_covid19_casos_long.csv" $@)
+REGION2=$(python "$BASE_DIR"/download_snapshot.py "$URL_BASE/COVID%2019/ccaa_covid19_fallecidos_long.csv" $@)
+COUNTRY=$(python "$BASE_DIR"/download_snapshot.py "$URL_BASE/COVID%2019/nacional_covid19.csv" $@)
+PREVIOUS=$(python "$BASE_DIR"/download_snapshot.py "https://open-covid-19.github.io/data/data.csv" $@)
+python "$BASE_DIR/parse_es_datadista_region.py" "$REGION1" "$REGION2" "$PREVIOUS"
+python "$BASE_DIR/parse_es_datadista_country.py" "$COUNTRY" "$PREVIOUS"
