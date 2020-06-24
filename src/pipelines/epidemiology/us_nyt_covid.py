f01755185aacadfe3c1983643cc7adee1dfaffc5
Oscar Wahltinez <oscar@wahltinez.org>
2020-06-23 21:47:23 -0400

    Fix NYC boroughs being reported as null county

--- src/pipelines/epidemiology/us_nyt_covid.py
+++ src/pipelines/epidemiology/us_nyt_covid.py
@@ -14,9 +14,9 @@
 
 from datetime import datetime
 from typing import Any, Dict, List
-from pandas import DataFrame, concat, merge
+from pandas import DataFrame, Series, concat, merge
+from lib.cast import safe_int_cast
 from lib.pipeline import DataSource
-from lib.utils import grouped_diff
 
 
 class NytCovidL2DataSource(DataSource):
@@ -29,8 +29,8 @@ class NytCovidL2DataSource(DataSource):
             columns={
                 "date": "date",
                 "state": "subregion1_name",
-                "cases": "confirmed",
-                "deaths": "deceased",
+                "cases": "total_confirmed",
+                "deaths": "total_deceased",
             }
         )
 
@@ -48,9 +48,35 @@ class NytCovidL2DataSource(DataSource):
         data["key"] = "US_" + data["subregion1_code"]
 
         # Now that we have the key, we don't need any other non-value columns
-        data = data[["date", "key", "confirmed", "deceased"]]
-        data = grouped_diff(data, ["key", "date"])
+        data = data[["date", "key", "total_confirmed", "total_deceased"]]
         return data
+
+
+def _distribute_cases(
+    data: DataFrame, populations: DataFrame, county_mask: Series, codes: List[str]
+):
+    """
+    This function is used to distribute cases from data across [codes] proportional to their
+    population relative to each other.
+
+    @return the data after confirmed and deceased counts have been distributed across [codes]
+    """
+    state_code = data[county_mask].iloc[0]["subregion1_code"]
+    populations = {fips: populations.loc[f"US_{state_code}_{fips}", "population"] for fips in codes}
+    populations = {fips: val / sum(populations.values()) for fips, val in populations.items()}
+    county_records = []
+    for _, record in data[county_mask].iterrows():
+        county_records += [
+            {
+                "date": record["date"],
+                "subregion2_code": fips,
+                "subregion1_code": record["subregion1_code"],
+                "total_confirmed": record["total_confirmed"] * relative_population,
+                "total_deceased": record["total_deceased"] * relative_population,
+            }
+            for fips, relative_population in populations.items()
+        ]
+    return concat([data[~county_mask], DataFrame.from_records(county_records)])
 
 
 class NytCovidL3DataSource(DataSource):
@@ -59,38 +85,58 @@ class NytCovidL3DataSource(DataSource):
     ) -> DataFrame:
 
         # Rename the appropriate columns
-        data = (
-            dataframes[0]
-            .rename(
-                columns={
-                    "date": "date",
-                    "state": "subregion1_name",
-                    "fips": "subregion2_code",
-                    "cases": "confirmed",
-                    "deaths": "deceased",
-                }
-            )
-            .drop(columns=["county"])
-            .dropna(subset=["subregion2_code"])
+        data = dataframes[0].rename(
+            columns={
+                "date": "date",
+                "state": "subregion1_name",
+                "fips": "subregion2_code",
+                "cases": "total_confirmed",
+                "deaths": "total_deceased",
+            }
         )
 
         # Add state code to the data
-        us_meta = aux["metadata"]
-        us_meta = us_meta[us_meta["country_code"] == "US"]
-        us_meta = us_meta[us_meta["subregion2_code"].isna()]
-        state_map = {
-            idx: code
-            for idx, code in us_meta.set_index("subregion1_name")["subregion1_code"].iteritems()
-        }
-        data["subregion1_code"] = data["subregion1_name"].apply(lambda x: state_map[x])
+        us_states = aux["metadata"]
+        us_states = us_states[us_states["country_code"] == "US"]
+        us_states = us_states[us_states.subregion2_code.isna()]
+        data = data.merge(us_states[["subregion1_code", "subregion1_name"]], how="left")
+
+        # NYC has 5 boroughs which are all being reported as the same county with a null FIPS value.
+        # To ensure that the sum of all counties is closer to the total numbers state-wide, we
+        # divide the cases and fatalities across all boroughs in a manner directly proportional to
+        # their population to "redistribute" the case counts.
+        populations = aux["demographics"].set_index("key")
+        nyc_counties = [
+            "36005",  # Bronx
+            "36047",  # Kings AKA Brooklyn
+            "36061",  # New York County AKA Manhattan
+            "36081",  # Queens
+            "36085",  # Richmond AKA Staten Island
+        ]
+        nyc_data_mask = (data.subregion1_code == "NY") & (data.county == "New York City")
+        data = _distribute_cases(data, populations, nyc_data_mask, nyc_counties)
+
+        # Do the same for Kansas City and its 4 counties
+        kc_counties = [
+            "29037",  # Cass County
+            "29047",  # Clay County
+            "29095",  # Jackson County
+            "29165",  # Platte County
+        ]
+        kc_data_mask = (data.subregion1_code == "MO") & (data.county == "Kansas City")
+        data = _distribute_cases(data, populations, kc_data_mask, kc_counties)
+
+        # Remove "unknown" counties
+        data = data[data.county != "Unknown"]
+
+        # Give records with null FIPS code a bogus code so they are reported as mismatches
+        # There should be none of these, but this will let us catch mismatches if the data changes
+        data.loc[data.subregion2_code.isna(), "subregion2_code"] = 0
 
         # Make sure the FIPS code is well-formatted
         data["subregion2_code"] = data["subregion2_code"].apply(lambda x: "{0:05d}".format(int(x)))
 
         # Manually build the key rather than doing automated merge for performance reasons
         data["key"] = "US_" + data["subregion1_code"] + "_" + data["subregion2_code"]
-
-        # Now that we have the key, we don't need any other non-value columns
-        data = data[["date", "key", "confirmed", "deceased"]]
-        data = grouped_diff(data, ["key", "date"])
+        data = data[["date", "key", "total_confirmed", "total_deceased"]]
         return data
