8c0f034eb17b5f6409425920a579399e3e7976ee
Mayank Daswani <mdaswani@google.com>
2020-07-15 15:45:23 +0100

    Address review feedback.

--- src/pipelines/epidemiology/ht_humdata.py
+++ src/pipelines/epidemiology/ht_humdata.py
@@ -15,7 +15,8 @@
 from typing import Dict
 from pandas import DataFrame, concat
 from lib.data_source import DataSource
-from lib.cast import safe_datetime_parse
+from lib.time import datetime_isoformat
+from lib.utils import table_rename
 
 
 class HaitiHumdataDataSource(DataSource):
@@ -26,36 +27,34 @@ class HaitiHumdataDataSource(DataSource):
         # They have different column-names, so we normalize and combine.
         dataframe = dataframes[0]
 
-        dataframe["data_asofMay5"] = dataframe["data_asofMay5"].drop(
-            columns=["Suspected cases", "Fatality rate"]
-        )
-        dataframe["data_fromMay6"] = dataframe["data_fromMay6"].drop(
-            columns=["New deaths (24h) Rate Of", "Case fatality rate", "New cases (24h)"]
-        )
-
         # Confirmed cases in asofMay5 is the same as Cumulative cases in fromMay6
         # Deaths in asofMay5 is the same as Cumulative deaths in fromMay6
-        dataframe["data_asofMay5"] = dataframe["data_asofMay5"].rename(
-            columns={
+        dataframe["data_asofMay5"] = table_rename(
+            dataframe["data_asofMay5"],
+            {
                 "Date": "date",
                 "Confirmed cases": "total_confirmed",
                 "Deaths": "total_deceased",
                 "Département": "match_string",
-            }
+            },
+            drop=True,
         )
 
-        dataframe["data_fromMay6"] = dataframe["data_fromMay6"].rename(
-            columns={
+        dataframe["data_fromMay6"] = table_rename(
+            dataframe["data_fromMay6"],
+            {
                 "Date": "date",
                 "Cumulative cases": "total_confirmed",
                 "Cumulative Deaths": "total_deceased",
                 "Département": "match_string",
-            }
+            },
+            drop=True,
         )
 
+        # Concatenate the two Series and drop the first row which is a column description.
         data = concat([dataframe["data_asofMay5"], dataframe["data_fromMay6"]]).drop(0)
 
-        data.date = data.date.apply(safe_datetime_parse)
+        data.date = data.date.apply(lambda x: datetime_isoformat(x, "%Y-%m-%d %H:%M:%S"))
 
         # Make sure all records have the country code
         data["country_code"] = "HT"
