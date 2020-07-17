a7192c481e0e15e848decbde7320a571bb72504f
Pranali Yawalkar <pranaliyawalkar@users.noreply.github.com>
2020-07-17 16:55:07 +0100

    Update ly_humdata.py

--- src/pipelines/epidemiology/ly_humdata.py
+++ src/pipelines/epidemiology/ly_humdata.py
@@ -42,7 +42,7 @@ class LibyaHumdataDataSource(DataSource):
         )
 
         # The first row is metadata info about column names - discard it
-        data = data[data.match_string != '#loc+name']
+        data = data[data.subregion2_name != '#loc+name']
       
         # Convert string numbers to int
         # Parse integers
